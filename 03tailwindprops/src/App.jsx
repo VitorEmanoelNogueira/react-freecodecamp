062748d2f75ddfb1deff6083ddee5b22e5f669cc
Vitor Emanoel da Silva Nogueira <vitoremanoelnogueira@gmail.com>
2024-12-19 18:52:20 -0300

    Tailwind and props practice

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl bg-green-500 p-3 rounded-md'>Vite with Tailwind</h1>
+      <Card username="Vitor" post='Web Developer' image='https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-in-headphones-showing-programming-process-on-a-laptop.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load'/>
+      <Card myArr={[1, 2, 3]} />
+      <Card />
+    </>
+  )
+}
+
+export default App
