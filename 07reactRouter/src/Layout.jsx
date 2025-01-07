509dada1b99a7bc6927f81cbeb8744f0afda3898
Vitor Emanoel da Silva Nogueira <vitoremanoelnogueira@gmail.com>
2025-01-07 14:13:50 -0300

    Fixed imports in Layout.jsx on project 07

--- 07reactRouter/src/Layout.jsx
+++ 07reactRouter/src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
-import Header from './components/header/Header'
-import Footer from './components/Footer/Footer'
+import Header from './components/header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 function Layout() {
   return (
